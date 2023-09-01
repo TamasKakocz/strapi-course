97dfdff8ceea7fe4e9756c953b45554aa6bb2f50
Tamas Kakocz <tamas.kakocz@varadium.com>
2023-09-01 14:17:32 +0300

    salon user added

---
+++ src/api/salonuser/routes/salonuser.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * salonuser router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::salonuser.salonuser');
---
+++ src/api/salonuser/services/salonuser.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * salonuser service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::salonuser.salonuser');
